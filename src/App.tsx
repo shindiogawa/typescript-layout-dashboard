b4d28aeb63f053ac4cfe2703c606eef6170ee563
Rodrigo Ogawa <rodrigoshindi@gmail.com>
2020-10-08 17:55:10 -0300

    Added styled-components type as a development mode

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Dashboard from './pages/Dashboard'
+import GlobalStyles from './styles/GlobalStyles'
+
+// import { Container } from './styles';
+
+const App: React.FC = () => {
+  return (
+    <>
+      <GlobalStyles />
+      <Dashboard />
+    </>
+  )
+}
+
+export default App
