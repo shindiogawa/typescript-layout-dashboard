e4750bb407222c267196f118f4496431f1fb9475
Rodrigo Ogawa <rodrigoshindi@gmail.com>
2020-10-08 20:37:52 -0300

    Created header contents

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import Layout from './components/Layout'
 import GlobalStyles from './styles/GlobalStyles'
 import { ThemeProvider } from 'styled-components'
 import dark from './styles/themes/dark'
-
+import Dashboard from './pages/Dashboard'
 const App: React.FC = () => {
   return (
     <ThemeProvider theme={dark}>
       <GlobalStyles />
-      <Layout />
+      <Layout>
+        <Dashboard />
+      </Layout>
     </ThemeProvider>
   )
 }
