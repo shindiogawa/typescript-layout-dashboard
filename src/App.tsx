87bfbed8c0b9d6fa8fe11627e5f221d443659b9e
Rodrigo Ogawa <rodrigoshindi@gmail.com>
2020-10-08 17:25:10 -0300

    Create the first components of the project

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-// import { Container } from './styles';
-
-const App: React.FC = () => {
-  return <h1>Hello World</h1>
-}
-
-export default App
---
+++ src/pages/List/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+// import { Container } from './styles';
+
+const List: React.FC = () => {
+  return <h1>List</h1>
+}
+
+export default List
