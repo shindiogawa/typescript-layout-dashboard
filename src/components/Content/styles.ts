e633ea7dbf34a0e5929dd63983370c80040000ed
Rodrigo Ogawa <rodrigoshindi@gmail.com>
2020-10-08 22:06:58 -0300

    Created cards

--- src/components/Content/styles.ts
+++ src/components/Content/styles.ts
@@ -5,4 +5,20 @@ export const Container = styled.div`
   color: ${props => props.theme.colors.white};
   background-color: ${props => props.theme.colors.primary};
   padding: 25px;
+
+  height: calc(100vh - 70px);
+  overflow-y: scroll;
+
+  ::-wekbit-scrollbar {
+    width: 10px;
+  }
+
+  ::-wekbit-scrollbar-thumb {
+    background-gcolor: ${props => props.theme.colors.secondary};
+    border-radius: 10px;
+  }
+
+  ::-wekbit-scrollbar-track {
+    background-gcolor: ${props => props.theme.colors.tertiary};
+  }
 `
--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import ContentHeader from '../../components/ContentHeader'
+import HistoryFinanceCard from '../../components/HistoryFinanceCard'
 import SelectInput from '../../components/SelectInput'
 
-import { Container } from './styles'
+import { Container, Content } from './styles'
 
 const List: React.FC = () => {
   const options = [
@@ -16,6 +17,14 @@ const List: React.FC = () => {
       <ContentHeader title="Saídas" lineColor="#E44C4E">
         <SelectInput options={options} />
       </ContentHeader>
+      <Content>
+        <HistoryFinanceCard
+          tagColor="#E44C4E"
+          title="Conta de Luz"
+          subtitle="27/07/2020"
+          amount="R$ 130,00"
+        />
+      </Content>
     </Container>
   )
 }
