4f3dab361ca1b4bb62aa311b8cdc52159d967d4b
Rodrigo Ogawa <rodrigoshindi@gmail.com>
2020-10-09 18:47:11 -0300

    Added wallet box and added count-up

---
+++ src/components/WalletBox/index.tsx
@@ -0,0 +1,56 @@
+import React, { useMemo } from 'react'
+
+import { Container } from './styles'
+import dolarImg from '../../assets/dollar.svg'
+import arrowUpImg from '../../assets/arrow-up.svg'
+import arrowDownImg from '../../assets/arrow-down.svg'
+
+import CountUp from 'react-countup'
+
+interface IWalletBoxProps {
+  title: string
+  amount: number
+  footerLabel: string
+  icon: 'dolar' | 'arrowUp' | 'arrowDown'
+  color: string
+}
+
+const WalletBox: React.FC<IWalletBoxProps> = ({
+  title,
+  amount,
+  footerLabel,
+  icon,
+  color
+}) => {
+  const iconSelected = useMemo(() => {
+    switch (icon) {
+      case 'dolar':
+        return dolarImg
+      case 'arrowUp':
+        return arrowUpImg
+      case 'arrowDown':
+        return arrowDownImg
+      default:
+        return undefined
+    }
+  }, [icon])
+
+  return (
+    <Container color={color}>
+      <span>{title}</span>
+      <h1>
+        <CountUp
+          end={amount}
+          prefix={'R$ '}
+          separator="."
+          decimal=","
+          decimals={2}
+        />
+      </h1>
+      <small>{footerLabel}</small>
+      <img src={iconSelected} alt={title} />
+    </Container>
+  )
+}
+
+export default WalletBox
