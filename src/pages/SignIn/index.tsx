23acf5becd580aa59d83384dbdbfcca6cc97696a
Rodrigo Ogawa <rodrigoshindi@gmail.com>
2020-10-10 19:19:57 -0300

    Created form loggin
    Created components for input and button

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,9 +1,24 @@
 import React from 'react'
-
-// import { Container } from './styles';
+import logoImg from '../../assets/logo.svg'
+import Input from '../../components/Input'
+import Button from '../../components/Button'
+import { Container, Logo, Form, FormTittle } from './styles'
 
 const SignIn: React.FC = () => {
-  return <h1>SignIn</h1>
+  return (
+    <Container>
+      <Logo>
+        <img src={logoImg} alt="Minha carteira" />
+        <h2>Minha Carteira</h2>
+      </Logo>
+      <Form onSubmit={() => {}}>
+        <FormTittle>Entrar</FormTittle>
+        <Input required type="email" placeholder="seu@email.com" />
+        <Input required type="password" placeholder="sua senha" />
+        <Button type="submit">Acessar</Button>
+      </Form>
+    </Container>
+  )
 }
 
 export default SignIn
