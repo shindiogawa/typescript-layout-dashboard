a34fe2428b3392dc67fd9545881e0e14e0dd9765
Rodrigo Ogawa <rodrigoshindi@gmail.com>
2020-10-10 18:42:03 -0300

    Added functionality to change the theme

--- src/components/MainHeader/index.tsx
+++ src/components/MainHeader/index.tsx
@@ -1,16 +1,29 @@
-import React, { useMemo } from 'react'
+import React, { useMemo, useState } from 'react'
 import emojis from '../../utils/emojis'
 import Toggle from '../Toggle'
 import { Container, Profile, Welcome, UserName } from './styles'
-
+import { useTheme } from '../../hooks/theme'
 const MainHeader: React.FC = () => {
+  const { toggleTheme, theme } = useTheme()
+  const [darkTheme, setDarkTheme] = useState(() => theme.title === 'dark')
+
+  const handleChangeTheme = () => {
+    setDarkTheme(!darkTheme)
+    toggleTheme()
+  }
+
   const emoji = useMemo(() => {
     const indice = Math.floor(Math.random() * emojis.length)
     return emojis[indice]
   }, [])
   return (
     <Container>
-      <Toggle />
+      <Toggle
+        labelLeft="Light"
+        labelRight="Dark"
+        checked={darkTheme}
+        onChange={handleChangeTheme}
+      />
 
       <Profile>
         <Welcome>Olá, {emoji}</Welcome>
